bab5f7e2e3bcb8e1635dfa9231a8e28484833cc6
Ícaro Heimig <icaro.mh@gmail.com>
2022-03-28 15:23:48 +0100

    add purchasable info for product detail

--- products-proxy/src/routes/product.ts
+++ products-proxy/src/routes/product.ts
@@ -4,12 +4,13 @@ export default function ProductRoute(request: Request): Promise<any> {
   const { searchParams } = new URL(request.url)
   const productId = searchParams.get('productId')
   const skuId = searchParams.get('skuId');
-
+  
   if (skuId) {
     return fetch(`${PRODUCT_URL}&skuId=${skuId}&productId=${productId}`)
       .then(response => response.json())
       .then((response: any) => {
         return {
+          purchasable: response.purchasable,
           onSale: response.onSale,
           salePriceFormatted: response.salePriceFormatted,
           listPriceFormatted: response.listPriceFormatted,
